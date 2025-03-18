b2e9fbca924e8a3728279a691dd52d9d97e368b9
Elena Abanina <esina.helena@gmail.com>
2025-03-18 13:12:41 +0300

    fix: domain name

--- app/js/dev-temp.js
+++ app/js/dev-temp.js
@@ -5,7 +5,7 @@ function devLinkUrl() {
     links.forEach(function(item){
         let thisLink = item.getAttribute('href');
         let linkDev = '/template-dev-webpack' + thisLink;
-        if ((thisDomainPage === 'dev.texteh.ru') && (thisLink[0] === '/')) {
+        if ((thisDomainPage === 'dev.texteh.ru' || thisDomainPage === 'landing.texteh.ru') && (thisLink[0] === '/')) {
             item.setAttribute('href', linkDev);
         }
     });
