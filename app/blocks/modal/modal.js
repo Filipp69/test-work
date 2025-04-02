0ab2ac18350bb22b3b2700e39aabec9c2d5c5a53
Filipp <orlov.center.moscow@gmail.com>
2025-04-02 10:02:18 +0300

    fix modals

--- app/blocks/modal/modal.js
+++ app/blocks/modal/modal.js
@@ -1,13 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
   new Modal();
-
-  const form = document.querySelector("[data-form='callback']");
-
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    if (!form.classList.contains("invalid")) {
-      showThanks(form);
-    }
-  });
 });
