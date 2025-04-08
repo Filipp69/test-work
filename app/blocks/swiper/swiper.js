f35aff4648929bbef5164b4d1afb900571d7293f
Filipp <orlov.center.moscow@gmail.com>
2025-04-08 17:46:32 +0300

    edit block our cases

--- app/blocks/swiper/swiper.js
+++ app/blocks/swiper/swiper.js
@@ -48,31 +48,61 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+
+
   if (document.querySelector('[data-swiper="sliderCases"]')) {
-    document.querySelectorAll('[data-swiper="sliderCases"]').forEach((swiperEl) => {
-        new Swiper(swiperEl, {
+    const paginationImages = Array.from(document.querySelectorAll('.swiper-cases-text .swiper-slide .cases-pagination img'))
+        .map(img => img.outerHTML);
+
+    const textSwiper = new Swiper('.swiper-cases-text', {
+        spaceBetween: 20,
+        slidesPerView: 1,
+        speed: 800,
+        allowTouchMove: false,
+        updateOnWindowResize: true,
+        pagination: {
+          el: '.cases-slider__pagination',
+          clickable: true,
+          renderBullet: function(index, className) {
+              const slides = document.querySelectorAll('.swiper-cases-text .swiper-slide');
+              const img = slides[index].querySelector('.cases-pagination img');
+              return `<span class="${className}">${img?.outerHTML || ''}</span>`;
+          }
+      }
+    });
+
+    const imageSwiper = new Swiper('.swiper-cases-image', {
+        spaceBetween: 10,
+        slidesPerView: 1,
+        updateOnWindowResize: true,
+        speed: 1600,
+        allowTouchMove: false,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+    });
+
+    document.querySelectorAll('.swiper-image').forEach(nestedSwiperEl => {
+        new Swiper(nestedSwiperEl, {
             spaceBetween: 10,
             slidesPerView: 1,
-            updateOnWindowResize: true,
+            allowTouchMove: true,
+            speed: 1000,
             navigation: {
-                nextEl: swiperEl.querySelector('.swiper-button-next'),
-                prevEl: swiperEl.querySelector('.swiper-button-prev'),
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
             },
+            nested: true
         });
     });
-  }
 
-  if (document.querySelector('[data-swiper="sliderCasesWarp"]')) {
-    document.querySelectorAll('[data-swiper="sliderCasesWarp"]').forEach((swiperEl) => {
-        new Swiper(swiperEl, {
-            spaceBetween: 10,
-            slidesPerView: 1,
-            updateOnWindowResize: true,
-            navigation: {
-                nextEl: swiperEl.querySelector('.swiper-button-next'),
-                prevEl: swiperEl.querySelector('.swiper-button-prev'),
-            },
-        });
+    textSwiper.on('slideChange', function() {
+      imageSwiper.slideTo(this.activeIndex);
+    });
+
+    imageSwiper.on('slideChange', function() {
+        textSwiper.slideTo(this.activeIndex);
     });
   }
 
